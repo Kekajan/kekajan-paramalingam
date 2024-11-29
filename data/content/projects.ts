1a6aaf3b595c329ca370f52c0690e349bf4dd690
Kekajan <kekajanpk4220@gmail.com>
2024-11-29 19:50:18 +0530

    initial commit

---
+++ data/content/projects.ts
@@ -0,0 +1,78 @@
+import { kebabCase, kebabArray } from "@/utils/utils";
+import { Project } from "types";
+
+const projects: Project[] = [
+  {
+    id: 0,
+    title: "OmniBiz",
+    desc: "Business Management System",
+    img: "/static/projects/omniBiz.png",
+    link: "https://github.com/Kekajan/omnibiz-frontend",
+    github: "https://github.com/Kekajan/omnibiz-frontend",
+    tags: ["NextJS", "Bootstrap", "Django", "MySQL"],
+  },
+  {
+    id: 1,
+    title: "CareerHub",
+    desc: "Job Portal Application to connect job seekers with employers",
+    img: "/static/projects/buildfaster.png",
+    link: "https://github.com/Kekajan/CareerHub-Backend",
+    github: "https://github.com/Kekajan/CareerHub-Backend",
+    tags: ["NextJS", "Tailwind CSS", "NodeJS", "MongoDB"],
+  },
+  {
+    id: 2,
+    title: "Vehicle Vista",
+    desc: "Platform which connects people and vehicle service stations",
+    img: "/static/projects/react-emoji-search.png",
+    link: "https://react-emoji-search.braydentw.vercel.app/",
+    github: "https://github.com/braydentw/react-emoji-search",
+    tags: ["React", "Bootstrap", "PHP", "MySQL"],
+  },
+  {
+    id: 3,
+    title: "PRM",
+    desc: "Task management system",
+    img: "/static/projects/bitcointemp.png",
+    link: "https://github.com/Kekajan/PRM",
+    github: "https://github.com/Kekajan/PRM",
+    tags: ["React", "Bootstrap", "Django", "MySQL"],
+  },
+  {
+    id: 4,
+    title: "VoteEASE",
+    desc: "Election voting management system",
+    img: "/static/projects/create-html-boilerplate.png",
+    link: "https://github.com/Kekajan/VoteEASE",
+    github: "https://github.com/Kekajan/VoteEASE",
+    tags: ["HTML", "TailwindCSS", "JSP", "JavaServlet", "MySQL"],
+  },
+  {
+    id: 5,
+    title: "Movie-Site",
+    desc: "Online movie ticket booking system",
+    img: "/static/projects/8ball-rust.png",
+    link: "https://github.com/Kekajan/Movie-Site",
+    github: "https://github.com/Kekajan/Movie-Site",
+    tags: ["HTML", "Bootstrap", "PHP", "MySQL"],
+  },
+  {
+    id: 6,
+    title: "Discover Lanka",
+    desc: "Tourism guidence system",
+    img: "/static/projects/dontleaveme.png",
+    link: "https://github.com/Kekajan/Discover_Lanka_FE_new",
+    github: "https://github.com/Kekajan/Discover_Lanka_FE_new",
+    tags: ["React Native", "Django", "MongoDB"],
+  },
+];
+
+export const allTags = [];
+
+projects.forEach((project) => {
+  project.tags.forEach((tag) => !allTags.includes(tag) && allTags.push(tag));
+});
+
+export const allKebabTags = allTags.map((tag) => kebabCase(tag));
+
+export default projects;
---
+++ data/global.ts
@@ -0,0 +1,106 @@
+type Route = {
+  title: string;
+  path: string;
+};
+
+type FooterCol = {
+  title: string;
+  links: {
+    name: string;
+    link: string;
+    icon?: string;
+    leavesWebsite: boolean;
+  }[];
+};
+
+type Footer = {
+  columns: FooterCol[];
+  support: {
+    buymeacoffee?: string;
+    paypal?: string;
+    message?: string;
+  };
+};
+
+export const routes: Route[] = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  // {
+  //   title: "Blog",
+  //   path: "/blog",
+  // },
+  {
+    title: "Projects",
+    path: "/projects",
+  },
+  {
+    title: "Designs",
+    path: "/designs",
+  },
+];
+
+export const footer: Footer = {
+  columns: [
+    {
+      title: "Pages",
+      links: [
+        {
+          name: "Home",
+          link: "/",
+          leavesWebsite: false,
+        },
+        // {
+        //   name: "Blog",
+        //   link: "/blog",
+        //   leavesWebsite: false,
+        // },
+        {
+          name: "Projects",
+          link: "/projects",
+          leavesWebsite: false,
+        },
+        {
+          name: "Designs",
+          link: "/designs",
+          leavesWebsite: false,
+        },
+      ],
+    },
+    {
+      title: "Social",
+      links: [
+        {
+          name: "Email",
+          link: "mailto:kekajanpk4220@gmail.com",
+          icon: "/static/icons/mail-f.svg",
+          leavesWebsite: true,
+        },
+        {
+          name: "GitHub",
+          link: "https://github.com/Kekajan",
+          icon: "/static/icons/github-f.svg",
+          leavesWebsite: true,
+        },
+        {
+          name: "LinkedIn",
+          link: "https://www.linkedin.com/in/kekajan-paramalingam-bb8a531a4",
+          icon: "/static/icons/linkedin-f.svg",
+          leavesWebsite: true,
+        },
+        {
+          name: "Instagram",
+          link: "https://dribbble.com/braydentw",
+          icon: "/static/icons/2227.jpg",
+          leavesWebsite: true,
+        },
+      ],
+    },
+  ],
+  support: {
+    // buymeacoffee: "braydenw",
+    // paypal: "braydentw",
+    // message: "I appreciate your support very much! 💙",
+  },
+};
